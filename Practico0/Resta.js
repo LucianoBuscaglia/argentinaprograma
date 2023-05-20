9e24d96d4f498d5243b146072ebf483822d2c563
LucianoBuscaglia <buscaluc@gmail.com>
2023-05-20 11:18:35 -0300

    Practico 0 Ejecicio 1A

---
+++ Practico0/Resta.js
@@ -0,0 +1,12 @@
+//PRACTICO N° 0 EJERCICIO 1 A
+
+//Dado dos números calcule la resta entre ambos; donde el primer número
+//es el minuendo y el segundo es el sustraendo. Imprima el resultado por
+//pantalla. Deberá guardar este programa en un archivo llamado: resta.js.
+//Ayuda: utilizar console.log.
+//● ¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2?
+
+let num1= 10;
+let num2=2;
+let resta = num1 - num2
+console.log(resta);
