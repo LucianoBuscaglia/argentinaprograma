c500ff879249b53d4ee21c0c02a0aa05077ef741
LucianoBuscaglia <buscaluc@gmail.com>
2023-05-20 11:27:32 -0300

    Practico 0 Ejercicio 2

---
+++ Practico0/areaRectangulo.js
@@ -0,0 +1,21 @@
+//PRACTICO N° 0 EJERCICIO 2
+
+// Escribir un programa JavaScript que calcule el área de un rectángulo. 
+//Deberá guardar este programa en un archivo llamado: areaRectangulo.js. Luego,
+//responder a las siguientes preguntas utilizando su código para obtener los
+//resultados. Ayuda: la ecuación del área de un rectángulo está dada por:
+//area = base * altura
+
+//a. ¿Cuál es el área de un rectángulo de base 5cm y altura 3cm?
+//b. ¿Cuál es el área de un rectángulo de base 12.5cm y altura 5.3cm?
+
+let base_rta_a = 5;
+let altura_rta_a = 3;
+let area_rta_a = base_rta_a * altura_rta_a;
+
+let base_rta_b = 12.5;
+let altura_rta_b = 5.3;
+let area_rta_b = base_rta_b * altura_rta_b;
+
+console.log("El área del rectángulo de base 5 cm y altura 3 cm es: " + area_rta_a);
+console.log("El área del rectángulo de base 12.5 cm y altura 5.3 cm es: " + area_rta_b);
