a0df87dec590a0a6364b7a9ff3d531eabccb20ae
LucianoBuscaglia <buscaluc@gmail.com>
2023-05-20 11:31:28 -0300

    Practico 0 Ejericio 4

---
+++ Practico0/Promedio3.js
@@ -0,0 +1,30 @@
+//PRACTICO N° 0 EJERCICIO 4
+
+//Escribir un programa JavaScript que calcule el promedio de 3 números. Deberá
+//guardar este programa en un archivo llamado: promedio3.js. Luego, responder
+//a las siguientes preguntas utilizando su código para obtener los resultados. La
+//ecuación del promedio entre 3 números está dada por:
+
+// promedio = ((x + y + z))/3
+
+//a. ¿Cuál es el promedio entre 5, 7 y 9?
+//b. ¿Cuál es el promedio entre -2 , -3, -8?
+//c. ¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+
+let x1 = 5;
+let y1 = 7;
+let z1 = 9;
+let promedio1 = ((x1 + y1 + z1))/3
+console.log("El promedio entre 5, 7 y 9 es: " + promedio1)
+
+let x2 = -2;
+let y2 = -3;
+let z2 = -8;
+let promedio2 = ((x2 + y2 + z2))/3
+console.log("El promedio entre -2 , -3 y -8 es: " + promedio2)
+
+let x3 = -1.2;
+let y3 = 5.3;
+let z3 = -3.3;
+let promedio3 = ((x2 + y3 + z3))/3
+console.log("El promedio entre -1.2, 5.3 y -3.3 es: " + promedio3)
