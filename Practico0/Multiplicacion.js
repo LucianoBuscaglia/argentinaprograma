887a4b3a8d452c57a22aa369d9a043e7df2d5978
LucianoBuscaglia <buscaluc@gmail.com>
2023-05-20 11:23:28 -0300

    Practico 0 Ejercicio 1B

---
+++ Practico0/Multiplicacion.js
@@ -0,0 +1,12 @@
+//PRACTICO N° 0 EJERCICIO 1 B
+
+//Dado tres números calcule la multiplicación entre ellos y lo imprima por 
+//pantalla. Deberá guardar este programa en un archivo llamado:
+//multiplicacion.js. Ayuda: utilizar console.log.
+//● ¿Qué valor devuelve su programa si los números son 2 *17 * 6?
+
+let num1=2;
+let num2=17;
+let num3=6;
+let multiplicacion = num1 * num2 * num3
+console.log(multiplicacion);
