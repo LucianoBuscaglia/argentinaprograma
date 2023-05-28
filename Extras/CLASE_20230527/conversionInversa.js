dd8f34896c608797b12731e74beb2e2f805f6969
LucianoBuscaglia <buscaluc@gmail.com>
2023-05-27 23:57:38 -0300

    Ejecicio N°2

---
+++ Extras/CLASE_20230527/conversionInversa.js
@@ -0,0 +1,9 @@
+// EJERCICIO PRACTICO 0 EXTRA N° 2
+
+//2.	Ya sabemos, gracias al ejercicio 3, convertir de Fahrenheit a Celsius. 
+//¿Cómo hacemos la conversión de Celsius a Fahrenheit?
+
+let gradosC = 35;
+let gradosFahrenheit = (gradosC * 9/5) + 32;
+
+console.log(gradosC + " grados Celsius son " + gradosFahrenheit + " grados Fahrenheit.");
