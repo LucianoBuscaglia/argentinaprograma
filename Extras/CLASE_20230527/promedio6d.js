0f6ba124ff033bf1992b7cc0eca826100142483f
LucianoBuscaglia <buscaluc@gmail.com>
2023-05-27 23:55:41 -0300

    Ejecicio N°1

---
+++ Extras/CLASE_20230527/promedio6d.js
@@ -0,0 +1,17 @@
+// EJERCICIO PRACTICO 0 EXTRA N° 1
+//1.	En el ejercicio 4 se pedía calcular el promedio de 3 números. 
+//Supongamos que ahora nos llegan otros tres datos y queremos calcular el promedio de los 6 números. 
+//¿Qué modificaciones debemos hacer al programa?
+
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+let num4 = 10;
+let num5 = 15;
+let num6 = 23;
+
+let sumatoria = num1 + num2 + num3 + num4 + num5 + num6;
+let promedio = sumatoria / 6;
+
+console.log("De acuerdo a los números: " + num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " +  num5 + " y " + num6);
+console.log("El promedio de los mismos es: " + promedio);
